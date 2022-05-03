e7c75df8c5ea0829a6401df88308fd21f6727ec2
MahtaFetrat <77fetrat@gmail.com>
2022-05-04 02:02:23 +0430

    add multitype location input

--- app/src/main/java/com/example/weatherforecast/Weather.java
+++ app/src/main/java/com/example/weatherforecast/Weather.java
@@ -2,11 +2,14 @@ package com.example.weatherforecast;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 public class Weather extends Fragment {
 
@@ -24,5 +27,17 @@ public class Weather extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //setup to show correct input fields
+        ((RadioGroup) view.findViewById(R.id.location_type_radio_group)).setOnCheckedChangeListener((group, checkedId) -> {
+            view.findViewById(R.id.coordinate_input_holder).setVisibility(checkedId == R.id.address_type ? View.GONE : View.VISIBLE);
+            view.findViewById(R.id.address_input_view).setVisibility(checkedId == R.id.address_type ? View.VISIBLE : View.GONE);
+        });
+
     }
 }
